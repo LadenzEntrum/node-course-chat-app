062a6b0ea92e807269b67c3a839439d2e244948a
TP <t@p.com>
2018-08-08 22:31:02 +0200

    Add form for messages and show incomming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -16,12 +16,21 @@ socket.on('disconnect',function ()  {
   console.log('Disconnected from Server fired by index.html');
 });
 
-//unten listener für new email
-// socket.on('newEmail', function (email) {
-//   console.log('New email',email);
-// });
-
 //unten listener für new message
 socket.on('newMessage', function (message) {
   console.log('New message',message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(){
+
+  });
 });
